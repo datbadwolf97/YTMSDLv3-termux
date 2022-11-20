a05bbcb89c70e54c3eaab87e4148cb2ca35c21d0
Irfan Togashi <96120619+datbadwolf97@users.noreply.github.com>
2022-11-20 07:32:59 +0700

    THe files is here!

---
+++ readme.md
@@ -0,0 +1,37 @@
+### Youtube Music Downloader version 3 - Termux.
+For Non Termux, click [here](http://github.com/datbadwolf97/YTMSDLv3).<br/>
+
+# Wait...3rd version?
+
+mmmmnyess...<br/>
+Well, I got bored from Bash Script and decided to move into Python....<br\>
+
+# Features?
+
+Same as [YTMSDLv2](https://github.com/datbadwolf97/YTMSDLv2) but in Python *and colors!*<br\>
+Oh also since it was in Python, it'll directly calls the functions from `yt-dlp` python package instead of command line.<br\>
+Ah yes, I've cut down some useless format such as `flac`, ya know, Youtube Music already lossy so it's pointless to convert into lossless format like flac, why's there has `wav` on it?<br/>
+It'll be nicer to put `wav` files into your favorite Digital Audio Workstation, right?<br/>
+# Depedencies?
+
+1. `colorama` from `pip` Package.
+2. [yt-dlp](http://github.com/yt-dlp/yt-dlp) of course.
+3. FFmpeg from package manager, either `apt` or `pacman` or `pamac`.
+4. That's all....
+
+## How to basic.
+
+```
+git clone http://github.com/datbadwolf97/YTMSDLv3-termux
+cd YTMSDLv3-termux
+```
+Then run the file using :
+```
+python3 ydp.py
+```
+# Question?
+
+Q : Where the downloaded file's located at?<br/>
+A : On your `~/Music` directory, right at your `Internal Storage`.<br/>
+
+### The End.
---
+++ ydp-trm.py
@@ -0,0 +1,76 @@
+###################################
+## YTMSDL VERSION 3 - NON TERMUX ##
+## Now with Python and colors!   ##
+## Creator : datbadwolf97        ##
+###################################
+from yt_dlp import YoutubeDL
+from colorama import Fore, Back
+import os
+
+while True:
+    ydl_mp3 = {
+        'outtmpl': '%(title)s.%(ext)s',
+        'paths' : {
+            'home' : '~/Music',
+        },
+        'format': 'm4a/bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    ydl_m4a = {
+        'outtmpl': '%(title)s.%(ext)s',
+        'paths' : {
+            'home' : '~/Music',
+        },
+        'format': 'm4a/bestaudio/best',
+    }
+
+    ydl_wav = {
+        'outtmpl' : '%(title)s.%(ext)s',
+        'paths' : {
+            'home' : '~/Music',
+        },
+        'format' : 'm4a/bestaudio/best',
+        'postprocessors' : [{
+            'key' : 'FFmpegExtractAudio',
+            'prefferedcodec' : 'wav',
+        }]
+    }
+    print(Fore.RED + Back.BLACK + 'Youtube Music Downloader III' + Fore.RESET + Back.RESET)
+    print(Fore.BLUE + 'Select a format below.' + Fore.RESET)
+    print(Fore.RESET + '1. MP3. \n2. M4A.\n3. WAV.\n4. Exit.')
+    inp1 = input(Fore.CYAN + 'Choose an option. : ' + Fore.RESET)
+    if inp1 == '1':
+        inp2 = input(Fore.RESET + 'Enter the link : ')
+        print(Fore.CYAN + 'Downloading MP3.' + Fore.RESET)
+        with YoutubeDL(ydl_mp3) as ydl:
+                error_code = ydl.download(inp2)
+        print('Done.')
+        os.system('sleep 1 && clear')
+        exit()
+    elif inp1 == '2':
+        inp3 = input(Fore.RESET + 'Enter the link : ')
+        print(Fore.GREEN + 'Downloading M4A.' + Fore.RESET)
+        with YoutubeDL(ydl_m4a) as ydl:
+            error_code = ydl.download(inp3)
+        print('Done.')
+        os.system('sleep 1 && clear')
+        exit()
+    elif inp1 == '3':
+        inp4 = input(Fore.RESET + 'Enter the link : ')
+        print(Fore.RED + 'Downloading WAV.' + Fore.RESET)
+        with YoutubeDL(ydl_wav) as ydl:
+            error_code = ydl.download(inp4)
+        os.system('sleep 1 && clear')
+        exit()
+    elif inp1 == '4':
+        print(Fore.RESET + 'Exiting...')
+        os.system('sleep 1 && clear')
+        exit()
+    else:
+        print(Fore.LIGHTRED_EX + 'Unrecognized option.' + Fore.RESET)
+        os.system('sleep 1 && clear')
+## Well, that's it for now...
